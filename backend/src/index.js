2c80a5c16835f08e48cbc4ad0977d17c7f828506
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-07-01 23:01:05 +0630

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
-console.log("starign...")
+import express from 'express'
+const app = express()
+
+
+app.get("/", async (req, res) => (
+    res.send("Server is alive ")
+))
+
+
+app.listen(3000, () => (
+    console.log("server is listening on port" + 3000)
+))
