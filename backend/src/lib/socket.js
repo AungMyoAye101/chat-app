2dbe1dd5e66f1f0547dbcb7f7a2935997f1f742f
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-07-03 13:01:01 +0630

    feat: integrate Tailwind CSS and refactor application structure
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Removed App.css and migrated styles to Tailwind CSS utility classes.
    - Updated App.tsx to utilize Tailwind CSS for styling.
    - Configured Vite to resolve paths for easier imports.
    - Updated TypeScript configuration to support Tailwind CSS and path aliases.

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,13 +1,17 @@
 import http from "http"
 import express from "express"
 import { Server } from "socket.io"
+
 const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: [process.env.FRONTEND_URL]
+        origin: ['http://localhost:5173']
     }
+})
+io.on("connection", (socket) => {
+    console.log(socket.id + "socket id")
 })
 
 export { io, server, app }
