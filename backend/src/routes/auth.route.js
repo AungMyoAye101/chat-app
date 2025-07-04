62aaea31ac76311aa348ae111972deb629ef2700
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-07-04 23:13:19 +0630

    feat: add user retrieval endpoint and token verification middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,18 @@
 import express from "express"
 import { createUser, login } from "../controllers/auth.js"
+import { verifyToken } from "../middleware/verify.js"
+import User from "../model/user.model.js"
 const router = express.Router()
 
 router.post("/register", createUser)
 router.post("/login", login)
+router.get("/me", verifyToken, async (req, res) => {
+    try {
+        const user = await User.findById(req.id).select("-password")
+        res.status(201).json({ message: "Get user", data: user })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 export default router
