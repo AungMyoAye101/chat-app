63362ab6993ed473e89cd9b55489aa122537d414
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-08-02 20:29:22 +0630

    FEAT: add group image upload route with validation and cloudinary integration

--- backend/src/routes/imageUpload.route.js
+++ backend/src/routes/imageUpload.route.js
@@ -2,6 +2,7 @@ import express from "express"
 import cloudinary, { upload } from "../lib/cloundinary.js"
 import mongoose from "mongoose"
 import User from "../model/user.model.js"
+import Group from "../model/group.model.js"
 import fs from "fs"
 const imageRouter = express.Router()
 
@@ -34,6 +35,41 @@ imageRouter.post('/upload/:id', upload.single('avatar'), async (req, res) => {
         await user.save()
         fs.unlinkSync(images.path)
         res.status(201).json(user)
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json("internal server error.")
+    }
+
+})
+imageRouter.post('/group/upload/:id', upload.single('avatar'), async (req, res) => {
+    console.log('uploading...')
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json("UserId is not valid")
+    }
+    const images = req.file
+    if (!images) {
+        res.status(400).json("Image not found.")
+    }
+    try {
+        const group = await Group.findById(id)
+        if (!group) {
+            res.status(400).json("Invalid userId!")
+        }
+        console.log(group.avatarPublicId)
+        if (group.avatarPublicId) {
+            await cloudinary.uploader.destroy(group.avatarPublicId)
+            console.log("delete old photo from cloudinary.")
+        }
+
+        const uploaded = await cloudinary.uploader.upload(images.path, { folder: "chat-app/profile" })
+        console.log("uploaded")
+        group.avatar = uploaded.secure_url;
+        group.avatarPublicId = uploaded.public_id
+        await group.save()
+        fs.unlinkSync(images.path)
+        res.status(201).json(group)
 
     } catch (error) {
         console.log(error.message)
