073dff76c8414e94fd18a5a2b4e4e23519e73e76
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-08-04 12:55:56 +0630

    FEAT:add  handle mebers add toggle function and render dynamic ui .

--- frontend/src/pages/Group.tsx
+++ frontend/src/pages/Group.tsx
@@ -55,42 +55,48 @@ const Group = () => {
     }
 
 
-}
 
-const handleAddMember = (userId: string) => {
-    if (!Array.data.members.includes(userId)) {
-        setData(pre => ({ ...pre, members: [...pre.members, userId] }))
+
+    const handleAddMember = (userId: string) => {
+        if (data.members.includes(userId)) {
+            const removeUser = data.members.filter(id => id !== userId)
+
+            setData(pre => ({ ...pre, members: removeUser }))
+        } else {
+            setData(pre => ({ ...pre, members: [...pre.members, userId] }))
+
+        }
+
     }
-}
-return (
-    <form onSubmit={handelSubmit} className='flex flex-col gap-4 bg-white p-6'>
-        <div>
-            <input type="name" placeholder='group name' name="name" onChange={(e) => setData(pre => ({ ...pre, 'name': e.target.value }))} className='w-full ' />
-        </div>
-        <div>
-            <h1 className="font-semibold text-lg font-serif mb-4">Add members</h1>
+    return (
+        <form onSubmit={handelSubmit} className='flex flex-col gap-4 bg-white p-6'>
             <div>
-                {
-                    members.map(user => (
-                        <div key={user._id} className="flex justify-between gap-4 px-4 py-1 hover:bg-blue-200" onClick={() => handleAddMember(user._id)}>
-                            <div className="flex items-center gap-4 ">
-
-                                <img src={user.avatar} alt={user.name + " profile photo"} className="w-12 h-12 rounded-full object-cover bg-gray-200 border border-purple-400" />
-                                <h1 className="font-medium text-lg font-serif">{user.name}</h1>
-                            </div>
-                            <button type="button" className="font-serif text-green-400  "
-                            >Add</button>
-                        </div>
-                    ))
-                }
+                <input type="name" placeholder='group name' name="name" onChange={(e) => setData(pre => ({ ...pre, 'name': e.target.value }))} className='w-full ' />
             </div>
-        </div>
-        <Button type='submit' text='Submit' />
-        {/* {
+            <div>
+                <h1 className="font-semibold text-lg font-serif mb-2">Add members</h1>
+                <div className="rounded-lg overflow-hidden">
+                    {
+                        members.map(user => (
+                            <div key={user._id} className={`flex justify-between gap-4 px-4 py-1 hover:bg-blue-200 ${data.members.includes(user._id) && "bg-green-100"}`} onClick={() => handleAddMember(user._id)}>
+                                <div className="flex items-center gap-4 ">
+
+                                    <img src={user.avatar} alt={user.name + " profile photo"} className="w-12 h-12 rounded-full object-cover bg-gray-200 border border-purple-400" />
+                                    <h1 className="font-medium text-lg font-serif">{user.name}</h1>
+                                </div>
+                                <button type="button" className="font-serif text-green-500  "
+                                >{data.members.includes(user._id) ? "Added" : "Add"}</button>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <Button type='submit' text='Submit' />
+            {/* {
                 errorMessage && <p className='text-sm text-red-400'>{errorMessage}</p>
             } */}
-    </form>
-)
+        </form>
+    )
 }
 
 export default Group
