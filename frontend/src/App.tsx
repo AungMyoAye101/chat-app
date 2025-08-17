44a3712e30006755107db150aa32622d8aff05a9
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-08-17 22:16:12 +0630

    FIX:fetch user slice.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,35 +16,53 @@ import DefaultChat from './pages/DefaultChat';
 import { useAuth } from './context/Auth.context';
 import UpdateUser from './pages/UpdateUser';
 import ImageUpload from './components/ImageUpload';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from './lib/auth/store';
+import { useEffect } from 'react';
+import { fetchUser } from './lib/auth/authSlice';
 interface ProtectedType {
   userId: string,
 
 }
 
-// const ProtectedRoute = ({ userId }: ProtectedType) => {
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
 
-//   if (!userId) {
-//     return <Navigate to={'/login'} replace />
-//   }
-//   return <Outlet />
-// }
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  console.log(user)
 
+  if (!user) {
+    // if not logged in, redirect to login
+    return <Navigate to="/login" replace />;
+  }
+
+  // if logged in, render the page
+  return <>{children}</>;
+};
 
 
 const App = () => {
-  const user = useAuth()
+  const dispatch: AppDispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(fetchUser())
+  }, [])
 
   return (
     <Routes>
 
 
-      <Route path='/' element={<Home />} >
+
+      <Route path='/' element={<ProtectedRoute><Home /> </ProtectedRoute>} >
         <Route index element={<DefaultChat />} />
         <Route path='/chat/user/:userId' element={<UserChat />} />
         <Route path='/chat/group/:groupId' element={<GroupChat />} />
 
+        \
       </Route>
+
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/user/:userId' element={<UserDeatil />} />
