1d06008e1868c4849a80a1f0c5e6255623db9483
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-08-22 22:48:35 +0630

    UPDATE: user deatil.

--- frontend/src/pages/UserDeatil.tsx
+++ frontend/src/pages/UserDeatil.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ImageUpload from '../components/ImageUpload'
 import ImageBox from '@/components/ImageBox'
+import { useAuth } from '@/lib/hooks/useAuth'
 
 const UserDeatil = () => {
     const [user, setUser] = useState<UserType>({
@@ -13,6 +14,8 @@ const UserDeatil = () => {
         avatar: "",
         lastSeen: ''
     })
+
+    const currUser = useAuth().user
     const [isOpenUploadImg, setIsOpenUploadImg] = useState(false)
     const { userId } = useParams()
 
@@ -36,21 +39,23 @@ const UserDeatil = () => {
     }
 
     return (
-        <section className="container px-4 py-6 bg-white shadow-lg border border-neutral-200  mt-4 ">
-            <div className='flex justify-between'>
+        <section className="max-w-xl mx-auto px-4 py-6 bg-white shadow-lg border border-neutral-200  mt-4 flex flex-col gap-4 ">
+            <h1 className='text-xl font-semibold '>User info</h1>
 
-                <div className='flex items-center gap-2'>
-                    <ImageBox avatar={user?.avatar!} name={user?.name!} size='lg' />
-                    <div>
-                        <h1 className='text-2xl font-bold'>{user?.name}</h1>
-                        <p className='font-medium text-neutral-600'>{user?.email}</p>
-                    </div>
-                </div>
-
+            <div className='flex items-center gap-2 border-b border-b-neutral-200 py-4'>
+                <ImageBox avatar={user?.avatar!} name={user?.name!} size='lg' />
                 <div>
-                    <button onClick={() => setIsOpenUploadImg(pre => !pre)} className='btn '>Set Profile</button>
+                    <h1 className='text-2xl font-bold'>{user?.name}</h1>
+                    <p className='font-medium text-neutral-600'>{user?.email}</p>
                 </div>
             </div>
+            {
+                user._id === currUser?._id ? <div>
+                    <button onClick={() => setIsOpenUploadImg(pre => !pre)} className='btn '>Set Profile</button>
+                </div> : <button>Send message</button>
+            }
+
+
             {
                 isOpenUploadImg && <ImageUpload id={userId!} onClose={onClose} img={user?.avatar!} type='user' />
             }
